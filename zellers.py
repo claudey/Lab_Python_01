480854c9e032f1509b6d15f80e4b2b8fa609d7ad
claudey <clauditey@gmail.com>
2012-06-20 11:07:30 +0000

    added the zeller.py file

---
+++ zellers.py
@@ -0,0 +1,6 @@
+print "Please enter the number for the month in which you were born\n"
+print "March = 1\nApril = 2\nMay = 3\nJune = 4\nJuly = 5\nAugust = 6"
+print "September = 7\nOctober = 8\nNovember = 9\nDecember = 10\nJanuary = 11\nFebruary = 12"
+A = raw_input ('Which number fits your mounth? ')
+
+B = raw_input ('Please enter the day of the month on which you were born eg, 31, 25, etc. ')
