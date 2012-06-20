3423d2b8d553695e2ab168471a80e8bb02e94195
claudey <clauditey@gmail.com>
2012-06-20 10:43:58 +0000

    Exercise 1 on day 3

---
+++ userinput.py
@@ -0,0 +1,7 @@
+fname = raw_input('Enter your fist name: ')
+lname = raw_input('Enter your last name: ')
+print "Enter your date of birth:"
+month = raw_input('\tMonth? ')
+day = raw_input('\tDay? ')
+year = raw_input('\tYear? ')
+print fname, lname, "was born on", month, day, year
